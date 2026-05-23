bccfed34c0a2caed5afd818628ee6f72a7656a72
ppranit0009 <ppranit0009@gmail.com>
2026-05-23 22:06:02 +0530

    Reorganize file structure: consolidate backend into api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    res.json({ status: 'OK', message: 'Server is running' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
