de801a238ed9f1f2a72bea5997451644eeddcce7
ppranit0009 <ppranit0009@gmail.com>
2026-06-09 08:54:41 +0530

    Add Analytics component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from "@vercel/analytics/react"
 
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
@@ -60,6 +61,7 @@ const App: React.FC = () => {
               <Toaster />
               <Sonner />
               <ScrollToTop />
+              <Analytics />
               <Routes>
                 {/* Public routes with layout */}
                 <Route element={<Layout />}>
